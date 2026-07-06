cf1d2f5d968e97ef7910d7028ad7108b17a42972
citycampuscu-sudo <citycampusc.u@gmail.com>
2026-07-06 20:48:13 +0300

    Add ChatMessage component for displaying messages

---
+++ project/src/components/ChatMessage.tsx
@@ -0,0 +1,27 @@
+import { Message } from "../../types/assistant";
+
+interface Props {
+  message: Message;
+}
+
+export default function ChatMessage({ message }: Props) {
+  const isUser = message.role === "user";
+
+  return (
+    <div
+      className={`flex ${
+        isUser ? "justify-end" : "justify-start"
+      }`}
+    >
+      <div
+        className={`max-w-[80%] rounded-2xl px-4 py-3 text-sm ${
+          isUser
+            ? "bg-[#2E3E87] text-white"
+            : "bg-white shadow"
+        }`}
+      >
+        {message.content}
+      </div>
+    </div>
+  );
+}
