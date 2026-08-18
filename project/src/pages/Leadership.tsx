e9942d085c2c01cd2e2aec9ec325b42ea708fe1f
citycampuscu-sudo <citycampusc.u@gmail.com>
2026-08-18 12:36:20 +0300

    Change mapping from sortedExecutiveLeaders to ministryLeaders

--- project/src/pages/Leadership.tsx
+++ project/src/pages/Leadership.tsx
@@ -954,7 +954,7 @@ export default function Leadership() {
 </div>
 
                         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-            {sortedExecutiveLeaders.map(
+            {ministryLeaders.map(
                             (leader: any, index: number) => (
                               <LeaderCard
                                 key={leader.name + '-' + leader.position}
